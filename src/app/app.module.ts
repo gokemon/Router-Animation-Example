99946f8bf2407ec195090c9e6d4687626dbc6a2b
gokemon <michael.of.gokey@gmail.com>
2017-05-18 17:53:16 -0400

    playing around with ng generate services and components and bootstrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductAddEditComponent } from './products/product-add-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductListComponent,
+    ProductAddEditComponent
   ],
   imports: [
     BrowserModule,
